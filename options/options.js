9bd166c8df243db428e262d33345748b9492f597
Crowdbotics <engineering@crowdbotics.com>
2023-02-23 00:21:01 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "dawn-feather-dev-65402",
+  url: "https://dawn-feather-dev-65402.botics.co",
+  api: "https://dawn-feather-dev-65402.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
